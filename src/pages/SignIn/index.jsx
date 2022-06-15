2fbe2575cecc7db230085067219e4032ef2dd276
SoftgenicPrashant <softgenics.prashant@gmail.com>
2022-06-16 01:02:32 +0530

    new commit

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -13,7 +13,10 @@ import FacebookIcon from "@mui/icons-material/FacebookTwoTone";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer , toast} from "react-toastify";
-
+import './Signin.css'
+import { Container, Row, Col , Image} from "react-bootstrap";
+import logoBlue from '../../assets/images/logo-blue.svg'
+import loginLady from "../../assets/images/sideimg.png";
 export default function SignIn() {
   const navigate = useNavigate();
   const [email, setEmail] = React.useState("");
@@ -59,53 +62,54 @@ export default function SignIn() {
       });
   };
   return (
-    <Box component="main" flexGrow={1} className="auth-signup">
-      <ToastContainer position="top-center" />
-      <CssBaseline />
-      <AuthTemplate>
-        <Box
-          className="signin-auth"
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            width: "340px"
-          }}
-          style={{
-            margin: "30px 122px 20px 220px"
-          }}
-        >
-          <Typography component="h1" variant="h4">
-            Sign in
-          </Typography>
-          <Typography component="p">
-            Enter your email and password for signing in.
-          </Typography>
-          <Typography component="p" mb={5}>
-            Thanks
-          </Typography>
-          <Box
-            component="form"
+    <>
+     <ToastContainer position="top-center" />
+    <CssBaseline />
+    <Container fluid className="signin">
+      
+      <Row>
+        <Col md={6} >
+        <div className="d-flex align-items-center position-absolute" style={{top: '10px' ,left: '10px'}}>
+        <img src={logoBlue} alt="logo" className="mx-3 mt-5" />
+        </div>
+        <Container className='mt-5 '>
+        <Row className='align-items-center justify-content-center form-cont'>
+        <Col md={8} className="signin-left">
+
+ 
+        <div className='mb-3'>
+        <h1 >
+        Sign in
+        </h1>
+          <p>Enter your email and password for signing in. <br></br>Thanks</p>
+            
+            </div>
+           <Box
+             component="form"
             noValidate
             onSubmit={handleSubmit}
-            sx={{ mt: 1 }}
+            
           >
-            <InputLabel htmlFor="email">Email Address</InputLabel>
+            
+           
             <TextField
               error={validationErrors}
-              margin="normal"
+             margin="normal"
               required
-              fullWidth
-              id="email"
-              name="email"
-              autoComplete="email"
+              label=""
+            fullWidth
+             id="email"
+             name="email"
+               autoComplete="email"
               autoFocus
-              value={email}
+             value={email}
+             variant="outlined"
               onChange={(e) => setEmail(e.target.value)}
-            />
-            <InputLabel htmlFor="password">Password</InputLabel>
-            <TextField
+             />
+             <InputLabel htmlFor="password">Password</InputLabel>
+             <TextField
               error={validationErrors}
-              margin="normal"
+              
               required
               fullWidth
               name="password"
@@ -116,46 +120,151 @@ export default function SignIn() {
               onChange={(e) => setPassword(e.target.value)}
             />
             {validationErrors && (
-              <InputLabel color="error" error>
-                {validationMessage}
+             <InputLabel color="error" error>
+               {validationMessage}
               </InputLabel>
             )}
             <Button
               type="submit"
-              fullWidth
+            fullWidth
               variant="contained"
-              sx={{ mt: 3, mb: 2, width: "100%" }}
-            >
-              LogIn
-            </Button>
-          </Box>
+               sx={{ mt: 3, mb: 2, width: "100%" }}
+             >
+             LogIn
+             </Button>
+           </Box>
           <Divider style={{ marginTop: "30px", marginBottom: "30px" }}>
             Sign in with
-          </Divider>
-          <Grid
-            container
-            direction="column"
-            sx={{ "& button": { m: 1, color: "black" } }}
-          >
-            <Button
-              sx={{ width: "100%" }}
-              variant="outlined"
-              size="large"
-              startIcon={<img src={GoogleIcon} alt="Google"></img>}
-            >
-              Google
-            </Button>
-            <Button
-              sx={{ width: "100%" }}
-              variant="outlined"
-              size="large"
-              startIcon={<FacebookIcon />}
-            >
-              Facebook
-            </Button>
-          </Grid>
-        </Box>
-      </AuthTemplate>
-    </Box>
+         </Divider>
+           <Grid
+             container
+             direction="column"
+             sx={{ "& button": { m: 1, color: "black" } }}
+           >
+             <Button
+               sx={{ width: "100%" }}
+               variant="outlined"
+               size="large"
+               startIcon={<img src={GoogleIcon} alt="Google" style={{height:"25px", width:"25px"}}></img>}
+             >
+               Google
+             </Button>
+             <Button
+               sx={{ width: "100%" }}
+               variant="outlined"
+               size="large"
+               startIcon={<FacebookIcon />}
+             >
+               Facebook
+             </Button>
+           </Grid>
+           </Col>
+        </Row>
+           </Container>
+        </Col>
+        <Col md={6} className="signin-right">
+          <Image src={loginLady} alt='loggin picture' fluid />
+        </Col>
+      </Row>
+    </Container>
+    </>
+    // <Box component="main" flexGrow={1} className="auth-signup">
+    //   <ToastContainer position="top-center" />
+    //   <CssBaseline />
+    //   {/* <AuthTemplate> */}
+    //     <Box
+    //       className="signin-auth"
+    //       sx={{
+    //         display: "flex",
+    //         flexDirection: "column",
+    //         width: "340px"
+    //       }}
+    //       style={{
+    //         margin: "30px 122px 20px 220px"
+    //       }}
+    //     >
+    //       <Typography component="h1" variant="h4">
+    //         Sign in
+    //       </Typography>
+    //       <Typography component="p">
+    //         Enter your email and password for signing in.
+    //       </Typography>
+    //       <Typography component="p" mb={5}>
+    //         Thanks
+    //       </Typography>
+    //       <Box
+    //         component="form"
+    //         noValidate
+    //         onSubmit={handleSubmit}
+    //         sx={{ mt: 1 }}
+    //       >
+    //         <InputLabel htmlFor="email">Email Address</InputLabel>
+    //         <TextField
+    //           error={validationErrors}
+    //           margin="normal"
+    //           required
+    //           fullWidth
+    //           id="email"
+    //           name="email"
+    //           autoComplete="email"
+    //           autoFocus
+    //           value={email}
+    //           onChange={(e) => setEmail(e.target.value)}
+    //         />
+    //         <InputLabel htmlFor="password">Password</InputLabel>
+    //         <TextField
+    //           error={validationErrors}
+    //           margin="normal"
+    //           required
+    //           fullWidth
+    //           name="password"
+    //           type="password"
+    //           id="password"
+    //           autoComplete="current-password"
+    //           value={password}
+    //           onChange={(e) => setPassword(e.target.value)}
+    //         />
+    //         {validationErrors && (
+    //           <InputLabel color="error" error>
+    //             {validationMessage}
+    //           </InputLabel>
+    //         )}
+    //         <Button
+    //           type="submit"
+    //           fullWidth
+    //           variant="contained"
+    //           sx={{ mt: 3, mb: 2, width: "100%" }}
+    //         >
+    //           LogIn
+    //         </Button>
+    //       </Box>
+    //       <Divider style={{ marginTop: "30px", marginBottom: "30px" }}>
+    //         Sign in with
+    //       </Divider>
+    //       <Grid
+    //         container
+    //         direction="column"
+    //         sx={{ "& button": { m: 1, color: "black" } }}
+    //       >
+    //         <Button
+    //           sx={{ width: "100%" }}
+    //           variant="outlined"
+    //           size="large"
+    //           startIcon={<img src={GoogleIcon} alt="Google"></img>}
+    //         >
+    //           Google
+    //         </Button>
+    //         <Button
+    //           sx={{ width: "100%" }}
+    //           variant="outlined"
+    //           size="large"
+    //           startIcon={<FacebookIcon />}
+    //         >
+    //           Facebook
+    //         </Button>
+    //       </Grid>
+    //     </Box>
+      // {/* </AuthTemplate> */}
+    // </Box>
   );
 }
